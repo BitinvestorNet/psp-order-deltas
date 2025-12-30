7a7a915f5354e2c1954dc415e4a004da7e4be54e
Mark Jones <mark@swapped.com>
2025-12-30 15:03:24 +0000

    feat: bash script to update, install, and run

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_status() {
+    if [ $? -ne 0 ]; then
+        echo "$1 failed!"
+        exit 1
+    fi
+}
+
+git pull
+check_status "Update"
+
+# use pip instead of apt, requires --break-system-packages 
+pip install --requirement /home/ubuntu/psp-order-deltas/requirements.txt --break-system-packages 
+check_status "Installation"
+
+# Run the script
+/usr/bin/python3 /home/ubuntu/psp-order-deltas/main.py
+check_status "Script execution"
